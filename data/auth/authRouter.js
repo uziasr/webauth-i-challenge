6692d64ac4452cf2c636a5c9ef5d78743950327e
Uzias Rivera <uziasrivera@gmail.com>
2019-11-12 15:06:38 -0800

    completed projected

--- data/auth/authRouter.js
+++ data/auth/authRouter.js
@@ -39,4 +39,22 @@ router.post('/login', (req, res) => {
     });
 });
 
+router.get('/logout', (req,res)=>{
+  if (req.session){
+    req.session.destroy(error=>{
+      if(error){
+        res
+        .status(500)
+        .json({
+          message:"you can check out any time you like, but you can never leave..."
+        })
+      } else{
+        res.status(200).json({message: "logged out successfully"})
+      }
+    });
+  }else{
+    res.status(500).json({message:"bye felicia"})
+  }
+})
+
 module.exports = router;
